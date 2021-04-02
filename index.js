7db29023c736bbd477d44b482f2dbad823d86649
surya-nagrajan <77264555+surya-nagrajan@users.noreply.github.com>
2021-04-02 09:43:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const mongodb = require("mongodb");
+var cors = require('cors')
+
+const mongoClient = mongodb.MongoClient;
+const objectId = mongodb.ObjectID;
+const app = express();
+
+const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017";
+const port = process.env.PORT || 4000;
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  try {
+    let clientInfo = await mongoClient.connect(dbUrl);
+    let db = clientInfo.db("order");
+    let data = await db.collection("data").find().toArray();
+    res.status(200).json(data);
+    clientInfo.close();
+  } catch (error) {
+    console.log(error);
+  }
+});
+app.post("/create_order", async (req, res) => {
+  try {
+    let client = await mongoClient.connect(dbUrl);
+    let db = client.db("order");
+    await db.collection("data").insertOne(req.body);
+    res.status(200).json({ message: "order placed" });
+    client.close();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+
+
+
+app.listen(port, () => console.log("App runs with 4000"));
